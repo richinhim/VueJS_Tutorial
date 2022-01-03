9388b328bd0d8ba502607402430783976e5d546f
richinhim <kwlsy666@naver.com>
2022-01-03 20:58:53 +0900

    Vue JS 2 Tutorial #9 - Computed Properties

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: '',
-        age: ''
+        
+        age: 20,
+        a: 0,
+        b: 0
     },
-    methods: {        
-       logName: function(){
-           console.log('you entered your name');
+    methods: {    
+    /*    
+       addToA: function (){
+           console.log('addToA');
+           return this.a + this.age;
        },
-       logAge: function(){
-        console.log('you entered your age');
-    }
+       addToB: function (){
+        console.log('addToB');
+        return this.b + this.age;
+       } */
+    
+    },
+    computed: {
+        addToA: function (){
+            console.log('addToA');
+            return this.a + this.age;
+        },
+        addToB: function (){
+         console.log('addToB');
+         return this.b + this.age;
+        }
     }
 });

80d66b0558c5822dfeb6ba88302edcde52d3a0a6
richinhim <kwlsy666@naver.com>
2022-01-07 06:42:52 +0900

    14.Multiple Vue Instances

---
+++ 14.Multiple Vue Instances/app.js
@@ -0,0 +1,35 @@
+var one  = new Vue({
+    el: '#vue-app-one',
+    data: {
+      title: 'Vue App One'
+    },
+    methods: {
+
+    },
+    computed:{
+       greet: function () {
+           
+           return 'Hello from app one :)'
+       } 
+    }    
+});
+
+var two  = new Vue({
+    el: '#vue-app-two',
+    data: {
+      title: 'Vue App Two'
+    },
+    methods: {
+        changeTitle: function (){
+
+            one.title = "Title changed";
+        }
+    },
+    computed:{
+        greet: function () {
+            return 'Yo dudes, this is app 2 speaking to ya :)'
+        } 
+    }    
+});
+
+two.title = "Changed from outside";
